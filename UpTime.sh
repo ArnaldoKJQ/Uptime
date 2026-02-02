efbcc01c2861372419f26fc919b7e877f1af6737
AKJQ <arnaldokoo@gmail.com>
2026-02-02 08:12:36 +0800

    Update UpTime.sh

--- UpTime.sh
+++ UpTime.sh
@@ -3,7 +3,7 @@
 URL="https://megahtex.com/"
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" --max-time 10 "$URL")
 
-if [[ "$STATUS" != "200" ]]; then
+if [[ "$STATUS" = "200" ]]; then
   MESSAGE="⚠️ Website issue\nURL: $URL\nStatus: $STATUS"
 
   curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_MT_TOKEN/sendMessage" \
